d2fc0daf758a761eb9460164cda598f9da62f88e
Ardtick <thetodca@gmail.com>
2025-08-22 21:18:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
         ]
       : []),
   ],
+  
+  base: "/mypavie/",  
+
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -25,7 +29,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
